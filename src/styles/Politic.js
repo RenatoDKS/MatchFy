4b26fbee13a2b4f78363f7e2a34885349ec10f7e
Ronaldo Alves <juninho_custodio@hotmail.com>
2019-09-07 14:48:31 -0300

    adb de

--- src/styles/Politic.js
+++ src/styles/Politic.js
@@ -7,13 +7,13 @@ const styles = StyleSheet.create({
         flex:1, flexDirection: "column", backgroundColor:'white'
     },
     text1: {
-        color:'black', marginLeft:hp('20%'), fontSize: hp('20%')
+        color:'black', marginLeft:hp('2%'), fontSize: hp('3%'), fontWeight: 'bold'
     },
     text2: {
-        textAlign: "justify", fontSize:hp('20%')
+        textAlign: "justify", fontSize:hp('3%'), marginLeft:hp('4%'), marginRight:hp('4%')
     },
     text3: {
-        color: 'blue', fontSize:hp('20%')
+        color: 'blue', fontSize:hp('3%'), marginLeft:hp('4%')
     },
 
 
